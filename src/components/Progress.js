702af74b03efb4159ccf78a6c45d5e6a6f8c4d83
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2024-01-16 02:33:06 +0100

    cleaned up the code by using CONTEXT API to review prop drilling

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,12 +1,37 @@
-import React from "react";
+// import React from "react";
 
-const Progress = ({
-  index,
-  numQuestions,
-  points,
-  maxPossiblePoints,
-  answer,
-}) => {
+// const Progress = ({
+//   index,
+//   numQuestions,
+//   points,
+//   maxPossiblePoints,
+//   answer,
+// }) => {
+//   return (
+//     <header className="progress">
+//       <progress max={numQuestions} value={index + Number(answer !== null)} />
+//       <p>
+//         Question <strong>{index + 1}</strong>/{numQuestions}
+//       </p>
+//       <p>
+//         <strong>{points}</strong>/{maxPossiblePoints}
+//       </p>
+//     </header>
+//   );
+// };
+
+// export default Progress;
+
+
+
+import React from "react";
+import { useQuiz } from "../context/QuizContext";
+
+const Progress = () => {
+  const {
+    state: { index, numQuestions, points, maxPossiblePoints, answer },
+  } = useQuiz();
+
   return (
     <header className="progress">
       <progress max={numQuestions} value={index + Number(answer !== null)} />
@@ -21,3 +46,4 @@ const Progress = ({
 };
 
 export default Progress;
+
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,29 @@
+// import React from "react";
+// import Options from "./Options";
+
+// const Question = ({ question, dispatch, answer }) => {
+//   return (
+//     <div>
+//       <h4>{question.question}</h4>
+//       <Options question={question} dispatch={dispatch} answer={answer} />
+//     </div>
+//   );
+// };
+
+// export default Question;
+
+
 import React from "react";
 import Options from "./Options";
+import { useQuiz } from "../context/QuixContext";
 
-const Question = ({ question, dispatch, answer }) => {
+const Question = () => {
+  const { questions, index } = useQuiz();
+  const question = questions.at(index);
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} dispatch={dispatch} answer={answer} />
+      <Options question={question} />
     </div>
   );
 };
